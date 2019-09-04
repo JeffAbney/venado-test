4c757cfaffb6a98747b5c710cc5146a7c975837e
JeffAbney <jeffmabney@gmail.com>
2019-09-04 12:37:25 -0500

    Create hardcode of API data

---
+++ data.js
@@ -0,0 +1,191 @@
+export { gamesDataFile };
+
+const gamesDataFile =
+{
+  "success": true,
+  "data": {
+    "games": [
+      {
+        "local": true,
+        "opponent": "Deportivo Saprissa",
+        "opponent_image": "https://upload.wikimedia.org/wikipedia/commons/e/e3/Saprissa_New_Logo.png",
+        "datetime": "2019-07-12T01:00:00+00:00",
+        "league": "Amistoso",
+        "image": "https://venados.dacodes.mx/img/usr/99f8469c13f74a8caa8860dfcc9f844d.jpg",
+        "home_score": 2,
+        "away_score": 2
+      },
+      {
+        "local": true,
+        "opponent": "Atlante",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos64x64/2/2.png",
+        "datetime": "2019-07-31T00:00:00+00:00",
+        "league": "Copa MX",
+        "image": "https://venados.dacodes.mx/img/usr/c1a148f279de4878a718f634b6aa2e44.jpg",
+        "home_score": 4,
+        "away_score": 1
+      },
+      {
+        "local": false,
+        "opponent": "Loros",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos64x64/145/145.png",
+        "datetime": "2019-08-03T22:00:00+00:00",
+        "league": "Ascenso MX",
+        "image": "https://venados.dacodes.mx/img/usr/40a243c79b61442086daef1de7f7f38b.jpg",
+        "home_score": 1,
+        "away_score": 2
+      },
+      {
+        "local": false,
+        "opponent": "FC Juárez",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos64x64/10725/10725.png",
+        "datetime": "2019-08-07T02:00:00+00:00",
+        "league": "Copa MX",
+        "image": "https://venados.dacodes.mx/img/usr/7a6c0eac94d74c6f9e769f35141628c1.jpg",
+        "home_score": 1,
+        "away_score": 1
+      },
+      {
+        "local": true,
+        "opponent": "U. A. Edo. Méx.",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos64x64/171/171.png",
+        "datetime": "2019-08-11T00:00:00+00:00",
+        "league": "Ascenso MX",
+        "image": "https://venados.dacodes.mx/img/usr/13fbd85b8b874f1195d82da90bfbb494.jpg",
+        "home_score": 1,
+        "away_score": 1
+      },
+      {
+        "local": true,
+        "opponent": "U. de G.",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos64x64/33/33.png",
+        "datetime": "2019-08-17T00:00:00+00:00",
+        "league": "Ascenso MX",
+        "image": "https://venados.dacodes.mx/img/usr/3abec1d5f5b84688910a4537517f51ee.jpg",
+        "home_score": 1,
+        "away_score": 0
+      },
+      {
+        "local": false,
+        "opponent": "Dorados",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos64x64/3111/3111.png",
+        "datetime": "2019-08-25T02:00:00+00:00",
+        "league": "Ascenso MX",
+        "image": "https://venados.dacodes.mx/img/usr/9aaeba76f8ee4ae78d88b59d5e8fa83c.jpg",
+        "home_score": 1,
+        "away_score": 0
+      },
+      {
+        "local": true,
+        "opponent": "Mineros",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos64x64/10449/10449.png",
+        "datetime": "2019-08-31T02:00:00+00:00",
+        "league": "Ascenso MX",
+        "image": "https://venados.dacodes.mx/img/usr/c4691566ae3145999b1b90c0b6dc32cd.jpg",
+        "home_score": 1,
+        "away_score": 2
+      },
+      {
+        "local": true,
+        "opponent": "FC Juárez",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos64x64/10725/10725.png",
+        "datetime": "2019-09-05T00:00:00+00:00",
+        "league": "Copa MX",
+        "image": "https://venados.dacodes.mx/img/usr/e0bdfd5dc5cf4ce486ea542cdb7b7d87.jpg",
+        "home_score": 0,
+        "away_score": 0
+      },
+      {
+        "local": false,
+        "opponent": "Cimarrones",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos64x64/10721/10721.png",
+        "datetime": "2019-09-15T03:00:00+00:00",
+        "league": "Ascenso MX",
+        "image": "https://venados.dacodes.mx/img/usr/b5c002d1a24843f3bbd22b76970fde65.jpg",
+        "home_score": 0,
+        "away_score": 0
+      },
+      {
+        "local": true,
+        "opponent": "Alebrijes",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos64x64/3088/3088.png",
+        "datetime": "2019-09-21T03:00:00+00:00",
+        "league": "Ascenso MX",
+        "image": "https://venados.dacodes.mx/img/usr/258a704317ae4e43ba36cecf78c9b969.jpg",
+        "home_score": 0,
+        "away_score": 0
+      },
+      {
+        "local": false,
+        "opponent": "Celaya F.C.",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos64x64/10726/10726.png",
+        "datetime": "2019-09-29T17:00:00+00:00",
+        "league": "Ascenso MX",
+        "image": "https://venados.dacodes.mx/img/usr/918dc5f24491404cacf3f6dc57dd148c.jpg",
+        "home_score": 0,
+        "away_score": 0
+      },
+      {
+        "local": false,
+        "opponent": "Club Atlético Zacatepec ",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos64x64/11221/11221.png",
+        "datetime": "2019-10-04T01:30:00+00:00",
+        "league": "Ascenso MX",
+        "image": "https://venados.dacodes.mx/img/usr/e095017f3e0149aab4ed85ace7dbcfee.jpg",
+        "home_score": 0,
+        "away_score": 0
+      },
+      {
+        "local": true,
+        "opponent": "TM Fútbol Club",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos/10962/10962.png",
+        "datetime": "2019-10-19T02:00:00+00:00",
+        "league": "Ascenso MX",
+        "image": "https://venados.dacodes.mx/img/usr/e2fd2d5e618a47acb8ed41bef7fbddd7.jpg",
+        "home_score": 0,
+        "away_score": 0
+      },
+      {
+        "local": false,
+        "opponent": "Cafetaleros",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos/11779/11779.png",
+        "datetime": "2019-10-27T01:00:00+00:00",
+        "league": "Ascenso MX",
+        "image": "https://venados.dacodes.mx/img/usr/16a997d826604339a1211646c0c3cefd.jpg",
+        "home_score": 0,
+        "away_score": 0
+      },
+      {
+        "local": true,
+        "opponent": "Atlante",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos64x64/2/2.png",
+        "datetime": "2019-11-02T02:00:00+00:00",
+        "league": "Ascenso MX",
+        "image": "https://venados.dacodes.mx/img/usr/5166905f90be465d90dd1d0c9cd2da65.jpg",
+        "home_score": 0,
+        "away_score": 0
+      },
+      {
+        "local": false,
+        "opponent": "Atlante",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos64x64/2/2.png",
+        "datetime": "2019-11-06T01:00:00+00:00",
+        "league": "Copa MX",
+        "image": "https://venados.dacodes.mx/img/usr/b855fead6f2a4d03b1bfb358cd10fc3f.jpg",
+        "home_score": 0,
+        "away_score": 0
+      },
+      {
+        "local": false,
+        "opponent": "Correcaminos",
+        "opponent_image": "https://s3.amazonaws.com/lmxwebsite/docs/archdgtl/AfldDrct/logos64x64/21/21.png",
+        "datetime": "2019-11-09T02:00:00+00:00",
+        "league": "Ascenso MX",
+        "image": "https://venados.dacodes.mx/img/usr/d92ea74c63b84c65802588994ef801b8.jpg",
+        "home_score": 0,
+        "away_score": 0
+      }
+    ],
+    "code": 200
+  }
+}
