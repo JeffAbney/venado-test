c05f15b187faef16cec978ddaae44242b4212255
JeffAbney <jeffmabney@gmail.com>
2019-09-04 12:38:48 -0500

    start building home screen

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { gamesDataFile } from '../../data.js';
+import venadosLogo from '/home/jeff/git_workspace/React-Projects/venado-test/assets/images/venados_escudo.jpg';
 
+var monthList;
+const games = gamesDataFile.data.games;
 
-export default () => (
-    <div className="home" id="home">
-      <p>Hello there</p>
-    </div>
-  )
+const gamesCopa = games.filter((game) => game.league === "Copa MX");
+const gamesAscenso = games.filter((game) => game.league === "Ascenso MX");
+
+//create array of objects like thesegames = {Agosto: [{game A}, {game B}], [Septiembre: {game C},]}
+const LeagueByMonth = function (league) {
+  monthList = {};
+  const mlist = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo ",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+  ];
+  let gameMonth = (dateTime) => mlist[new Date(dateTime).getMonth()];
+
+  league.forEach((el) => {
+    console.log(el.datetime);
+    if (!monthList[gameMonth(el.datetime)]) {
+      monthList[gameMonth(el.datetime)] = [el];
+    } else {
+      monthList[gameMonth(el.datetime)].push(el);
+    }
+  })
+  return monthList;
+}
+
+class Home extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      league: "ascenso"
+    }
+  }
+
+  render() {
+    return (
+      <div className="home" id="home">
+        <div className="header-home">
+          <img className="home-head-image" src={venadosLogo} />
+        </div>
+        <div className="selector-container">
+          <div className="selector">
+            <p className="selector-text" onClick={this.showCopa}>COPA MX</p>
+          </div>
+          <div className="selector">
+            <p className="selector-text" onClick={this.showAscenso}>ASCENSO MX</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home;
