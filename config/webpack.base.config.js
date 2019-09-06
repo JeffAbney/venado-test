4a16ed69ab083aab9ef471817fee23b09f149cf5
JeffAbney <jeffmabney@gmail.com>
2019-09-06 10:12:09 -0500

    Add loader for fonts

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -11,54 +11,60 @@ const APP_DIR = path.resolve(__dirname, '../src');
 module.exports = env => {
   const { PLATFORM, VERSION } = env;
   return merge([
-      {
-        entry: ['@babel/polyfill', APP_DIR],
-        module: {
-          rules: [
-            {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              use: {
-                loader: 'babel-loader'
+    {
+      entry: ['@babel/polyfill', APP_DIR],
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader'
+            }
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+              'css-loader',
+              'sass-loader'
+            ]
+          },
+          {
+            test: /\.css$/,
+            use: [
+              PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+              'css-loader'
+            ]
+          },
+          {
+            test: /\.(png|jpg|gif|ttf|wav|mp3)$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {}
               }
-            },
-            {
-              test: /\.scss$/,
-              use: [
-                PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
-                'css-loader',
-                'sass-loader'
-              ]
-            },
-            {
-              test: /\.css$/,
-              use: [
-                PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
-                'css-loader'
-              ]
-            },
-            {
-              test: /\.(png|jpg|gif|ttf|wav|mp3)$/,
-              use: [
-                {
-                  loader: 'file-loader',
-                  options: {}
-                }
-              ]
-          }
-          ]
-        },
-        plugins: [
-          new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: './index.html'
-          }),
-          new webpack.DefinePlugin({ 
-            'process.env.VERSION': JSON.stringify(env.VERSION),
-            'process.env.PLATFORM': JSON.stringify(env.PLATFORM),
-          }),
-          new CopyWebpackPlugin([ { from: 'src/static' } ]),
+            ]
+          },
+          {
+            test: /\.(gif|eot|woff|woff2|ttf|svg)$/,
+            loaders: [
+              'url-loader'
+            ]
+          },
         ]
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          filename: './index.html'
+        }),
+        new webpack.DefinePlugin({
+          'process.env.VERSION': JSON.stringify(env.VERSION),
+          'process.env.PLATFORM': JSON.stringify(env.PLATFORM),
+        }),
+        new CopyWebpackPlugin([{ from: 'src/static' }]),
+      ]
     }
   ])
 };
