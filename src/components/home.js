9e361bb80eda230b6d9f1eed04e2a28226b58f0f
JeffAbney <jeffmabney@gmail.com>
2019-09-09 10:31:40 -0500

    Add games lists and calendar

--- src/components/home.js
+++ src/components/home.js
@@ -22,18 +22,95 @@ let getGamesAscenso = () =>
     :
     gamesAscenso;
 
+const CalendarArrayCopa = {
+  "Julio":
+    [
+      "https://calendar.google.com/event?action=TEMPLATE&tmeid=NGZkZzAxcHAzYWhmMnFhYzBibHBvcGUzcDUgZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com"
+    ],
+  "Agosto":
+    [
+      "https://calendar.google.com/event?action=TEMPLATE&tmeid=NWZmdnBpa3FmMzFvNzMwOHZwZTNwNHRubXQgZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com"
+    ],
+  "Septiembre":
+    [
+      "https://calendar.google.com/event?action=TEMPLATE&tmeid=MmQzcW8wdW9lZGdyOWZzZDQ4NWY5ZTc2dm0gZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com"
+    ],
+  "Noviembre":
+    [
+      "https://calendar.google.com/event?action=TEMPLATE&tmeid=NW8xcjc0bTFyYXNqcWttbmduc3Q0ZTlzdGIgZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com"
+    ]
+}
+
+const CalendarArrayAscenso = {
+  "Agosto":
+    [
+      "https://calendar.google.com/event?action=TEMPLATE&tmeid=M2VqdDMya3JpOWdpdXJhOTVwZXA3b2xkdTQgZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com",
+      "https://calendar.google.com/event?action=TEMPLATE&tmeid=MjBnaXZodmoxZDFhazI5dXJtMTc4MW5hcmIgZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com",
+      "https://calendar.google.com/event?action=TEMPLATE&tmeid=M2ZvM2xudnVvbHRicGQ0MGk3NWVzZjljcGYgZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com",
+      "https://calendar.google.com/event?action=TEMPLATE&tmeid=Mmh0aHZlMzI0bzdnZWVrbXJyam1vb204aHUgZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com",
+      "https://calendar.google.com/event?action=TEMPLATE&tmeid=NThwbjc4dTRnbTQxbmhqY2Voa2s3ZWVybGIgZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com"
+    ],
+  "Septiembre":
+    [
+      "https://calendar.google.com/event?action=TEMPLATE&tmeid=MjJxNzEzaTl1MDNzdjRlMnE5ZnY2cjAxZTUgZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com",
+      "https://calendar.google.com/event?action=TEMPLATE&tmeid=NGhnZTRoNXZxNWgzMHIxMG1wcXNmZXI3dDQgZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com",
+      "https://calendar.google.com/event?action=TEMPLATE&tmeid=M2R0dDFhcTc3ZGp1NXR0bTdxNGs0c29icmUgZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com",
+    ],
+  "Octubre": [
+    "https://calendar.google.com/event?action=TEMPLATE&tmeid=M2R0dDFhcTc3ZGp1NXR0bTdxNGs0c29icmUgZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com",
+    "https://calendar.google.com/event?action=TEMPLATE&tmeid=N3AxYmN2Mjhxb282Z2NzZHI3cGZqOXV0OGogZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com",
+    "https://calendar.google.com/event?action=TEMPLATE&tmeid=MXRqN2JiN3ZuZDJzbGcwZWQ0YW5mNW9pcHIgZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com",
+  ],
+  "Noviembre": [
+    "https://calendar.google.com/event?action=TEMPLATE&tmeid=NHZiMnYxNTkwZWE1aW1ybDQ1NzNmamIydWQgZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com",
+    "https://calendar.google.com/event?action=TEMPLATE&tmeid=M21obWFsZG1hMThuMDY4cDVtMDZ0dDg5M3YgZ2pqbW5sazQ0NXY1NmM5bGgyaTBqaDg0czRAZw&tmsrc=gjjmnlk445v56c9lh2i0jh84s4%40group.calendar.google.com"
+  ]
+
+}
+
+
+
 class Home extends Component {
 
   constructor(props) {
     super(props);
+    this.selectorRef = React.createRef();
 
     this.state = {
-      league: getGamesAscenso(),
+      league: "ASCENSO MX",
+      sticky: ""
     }
 
     this.changeLeague = this.changeLeague.bind(this);
     this.gameList = this.gameList.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
   }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(event) {
+    const selector = this.selectorRef.current;
+    var sticky = selector.offsetTop;
+
+    if (this.state.sticky === 'sticky' && window.pageYOffset < 140) {
+      console.log('window is', window.pageYOffset);
+      console.log("sticky is", sticky);
+      this.setState({
+        sticky: "",
+      })
+    } else if (window.pageYOffset >= sticky - 39 ) {
+        this.setState({
+          sticky: "sticky",
+        })
+      }
+    }
+  
 
   //create list of objects like thesegames = {Agosto: [{game A}, {game B}], [Septiembre: {game C},]}
   CreateLeagueByMonth(league) {
@@ -55,9 +132,8 @@ class Home extends Component {
     ];
 
     let gameMonth = (dateTime) => mlist[new Date(dateTime).getMonth()];
-
-
-    league.forEach((el) => {
+    let leagueGames = () => league === "ASCENSO MX" ? getGamesAscenso() : getGamesCopa();
+    leagueGames().forEach((el) => {
       if (!monthList[gameMonth(el.datetime)]) {
         monthList[gameMonth(el.datetime)] = [el];
       } else {
@@ -92,7 +168,9 @@ class Home extends Component {
           {leagueByMonth[month].map((game, i) =>
             <div className='game' id={i} key={`game-${i}`}>
               <div className='calendar-container'>
-                <img className='calendar-image' src={calendar} />
+                <a target="_blank" href={this.state.league === "COPA MX" ? CalendarArrayCopa[month][i] : CalendarArrayAscenso[month][i]}>
+                  <img className='calendar-image' src={calendar} />
+                </a>
                 <p className='day-number'>{getDayNum(leagueByMonth[month][i].datetime)}</p>
                 <p className='day-word'>{getDayName(leagueByMonth[month][i].datetime)}</p>
               </div>
@@ -120,18 +198,9 @@ class Home extends Component {
 
   changeLeague(league) {
     if (this.state.league !== league) {
-      if (league === "ASCENSO MX") {
-        this.setState({
-          league: getGamesAscenso()
-        })
-      } else if (league === "COPA MX") {
-        this.setState({
-          league: getGamesCopa()
-        })
-      } else {
-        console.log("Invalid League");
-      }
-
+      this.setState({
+        league: league
+      })
     } else {
       console.log("Already there.")
     }
@@ -143,7 +212,8 @@ class Home extends Component {
         <div className="header-home">
           <img className="home-head-image" src={venadosLogo} />
         </div>
-        <div className="selector-container">
+        <div className={`selector-container ${this.state.sticky}`} ref={this.selectorRef}>
+
           <div className="selector">
             <p className="selector-text" onClick={() => this.changeLeague('COPA MX')}>COPA MX</p>
           </div>
